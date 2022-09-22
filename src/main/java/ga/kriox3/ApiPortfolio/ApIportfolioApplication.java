25fa7654add8c6271bda463402c3528e9d9540aa
kriox3 <kkrriiooxx33@gmail.com>
2022-09-22 09:15:37 -0300

    SpringInitializer

---
+++ src/main/java/ga/kriox3/ApiPortfolio/ApIportfolioApplication.java
@@ -0,0 +1,13 @@
+package ga.kriox3.ApiPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/kriox3/ApiPortfolio/ApIportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.kriox3.ApiPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
