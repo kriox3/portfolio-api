928b8fb5422f44ad5e1da830006ef9de65919978
kriox3 <kkrriiooxx33@gmail.com>
2022-10-13 16:39:21 -0300

    CRUDcontactoINCOMPLETO

--- src/main/java/ga/kriox3/ApiPortfolio/controller/RedesController.java
+++ src/main/java/ga/kriox3/ApiPortfolio/controller/RedesController.java
@@ -2,8 +2,10 @@ package ga.kriox3.ApiPortfolio.controller;
 
 import ga.kriox3.ApiPortfolio.model.redes;
 import ga.kriox3.ApiPortfolio.service.IRedesService;
+import ga.kriox3.ApiPortfolio.controller.PersonaController;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class RedesController {
 
     @Autowired
     private IRedesService interRedes;
+    @Autowired
+    private PersonaController persController;
 
     @GetMapping("redeses/traer")
     public List<redes> getRedes() {
@@ -26,6 +31,9 @@ public class RedesController {
 
     @PostMapping("redeses/crear")
     public String createRedes(@RequestBody redes nets) {
+        if(nets.getPersona() == null){
+            nets.setPersona(persController.devolverCliente());
+        }
         interRedes.saveRedes(nets);
         return "La red fue creada correctamente";
     }
